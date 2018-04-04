8d706327cb4067e9ac2725a3ae90281c1381b2d9
Charpell <charpellumeh@gmail.com>
2018-04-04 12:16:45 +0100

    Complete Project using only Promises

---
+++ currency-convert.js
@@ -0,0 +1,49 @@
+// USD CAD 23
+// 23 USD is worth 28 CAD. You can spend these in the following countries
+
+const axios = require('axios');
+
+const getExchangeRate = (from, to) => {
+  return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
+    return response.data.rates[to]
+  })
+}
+
+getExchangeRate('USD', 'EUR').then((rate) => {
+  console.log(rate)
+})
+
+
+const getCountries = (currencyCode) => {
+  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+    return response.data.map((country) => country.name)
+  })
+}
+
+getCountries('ngn').then((countries) => {
+  console.log(countries)
+})
+
+
+// Using Promises
+const convertCurrency = (from, to, amount) => {
+  let countries;
+  return getCountries(to).then((tempCountries) => {
+    countries = tempCountries
+    return getExchangeRate(from, to);
+  }).then((rate) => {
+    const exchangeAmount = amount * rate;
+
+    return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`
+  })
+}
+
+
+convertCurrency('CAD', 'USD', 100).then((status) => {
+  console.log(status)
+})
+
+
+
+
+
